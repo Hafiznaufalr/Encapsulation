cde3f84ccbeed5bdfc955cf13c7135dbffd93309
Hafiznaufalr <hafiz.nr5758@gmail.com>
2018-08-28 14:19:18 +0700

    first commit

---
+++ src/encapsiswa/EncapSiswa.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package encapsiswa;
+
+/**
+ *
+ * @author Hafiz
+ */
+public class EncapSiswa {
+    private String name;
+    private String address;
+    private int absen ;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public int getAbsen() {
+        return absen;
+    }
+
+    public void setAbsen(int absen) {
+        this.absen = absen;
+    }
+
+   
+    
+    
+    
+
+
+    
+}
